55fb1c52b6b5d39df99fefbab334111154c94435
caoyilong <caoyilong@idaoben.com>
2019-08-28 17:24:27 +0800

    提交初版，只完成了流控和降级规则接入nacos

---
+++ src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/NacosConstant.java
@@ -0,0 +1,27 @@
+package com.alibaba.csp.sentinel.dashboard.rule.nacos;
+
+/**
+ * @author caoyilong
+ * @description
+ * @package com.alibaba.csp.sentinel.dashboard.rule.nacos
+ * @date 2019/08/27 16:17
+ */
+public class NacosConstant {
+
+    /**
+     *  流控相关的
+     */
+    public final static String FLOW_RULE_NACOS_PROVIDER = "flowRuleNacosProvider";
+
+    public final static String FLOW_RULE_NACOS_PUBLISHER = "flowRuleNacosPublisher";
+
+    /**
+     * 降级规则
+     */
+    public final static String DEGRADE_RULE_NACOS_PROVIDER="degradeRuleNacosProvider";
+
+    public final static String DEGRADE_RULE_NACOS_PUBLISHER="degradeRuleNacosPublisher";
+
+
+
+}
